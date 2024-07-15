cc9996edf0493943e1fde8424e9a15d803017426
Mahmood <mahmood19971@live.com>
2024-07-15 10:20:15 +0200

    03 Instanzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,10 +4,17 @@ public class App {
     
     public static void main(String[] args) {
 
+        // System.out.println(new Cat());
+    
+       Cat cat = new Cat(); 
+       System.out.println("Blick von aussen " + cat);
+       cat.tellYourAddress();
 
-    }
 
-   
+       Cat cat2 = new Cat();
+       System.err.println("Blick von aussen " + cat2);
+        cat2.tellYourAddress();
+
+   }
 
 }
-
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Cat {
+
+   void tellYourAddress(){
+    System.out.println("Blick von innen: " + this);
+
+}
+
+    
+}
